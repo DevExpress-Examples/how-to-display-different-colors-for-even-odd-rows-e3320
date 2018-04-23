a056a8ddc096cdd3fe846afab4d6cb005089c91c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/SLGridExample/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿// Developer Express Code Central Example:
+// How to display different colors for even/odd rows
+// 
+// This sample shows how to change the background color for even and odd rows
+// separately. It is necessary to create custom data row template and bind its
+// background to the EvenRow property via converter.
+// 
+// You can find sample updates and versions for different programming languages here:
+// http://www.devexpress.com/example=E3320
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace SLGridExample {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+        }
+    }
+}
